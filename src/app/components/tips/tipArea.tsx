247578dc66bcfc40d6071aba1388b45a902856ad
Carl <me@mrcarllister.co.uk>
2024-06-20 13:25:31 +0100

    updates for content

--- src/app/components/tips/tipArea.tsx
+++ src/app/components/tips/tipArea.tsx
@@ -4,13 +4,23 @@ export default function tipArea(props: { content: { type: string, html: string }
 
 
     return (
-        <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6 prose xl:prose-lg">
+        <div className="px-4 pt-6 pb-12 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6 prose xl:prose-lg">
             {contentArray.map((detail,id) => (
-                <div id={`section-${id}`} key={id} className={detail.type == 'bonus' ? 'bg-secondary-200 rounded-md p-8 flex justify-center items-center' : ''}>
-                    <div  dangerouslySetInnerHTML={ { __html: detail.html } } />
-                    <hr/>
-                </div>
-            ))}
+
+                (['bonus', undefined].includes(detail.type)) ?
+                    <div id={`section-${id}`} key={id} className={detail.type == 'bonus' ? 'bg-secondary-200 rounded-md p-8 flex justify-center items-center' : ''}>
+                        <div  dangerouslySetInnerHTML={ { __html: detail.html } } />
+                        <hr/>
+                    </div>
+                
+                : 
+                    <blockquote id={`section-${id}`} key={id}>
+                        <div dangerouslySetInnerHTML={ { __html: detail.html } } />
+                    </blockquote>
+           
+        ))}
+        <hr/>
+            <p>Thanks for reading!</p>
         </div>
         
     )
