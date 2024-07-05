988f7c268c2916270c1d78588491a9378813f878
Carl <me@mrcarllister.co.uk>
2024-07-05 14:32:52 +0100

    new fields

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    webpack: (cfg) => {
+      cfg.module.rules.push(
+        {
+          test: /\.md$/,
+          loader: 'frontmatter-markdown-loader',
+          options: { mode: ['react-component'] },
+        }
+      );
+      return cfg;
+    }
+  };
+  
+  export default nextConfig;
